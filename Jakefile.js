7a0e21620fd3aecd71e0abaa4f71f2e033b908e5
javieralmendrote <javier.almendrote@gmail.com>
2014-05-16 20:52:16 +0200

    Jakefile modified. Not excludes build folder.

--- Jakefile.js
+++ Jakefile.js
@@ -1,8 +1,21 @@
+/*global desc, task, jake, fail, complete */
+
+"use strict";
+
 task("default", ["Lint"]);
 
 desc("Lint everything");
 
 task("Lint", [], function() {
     var lint = require("./build/lint/lint_runner.js");
-    lint.validateFile("Jakefile.js", {}, {});
+
+    var files = new jake.FileList();
+    files.include("**/*.js");
+    files.exclude("node_modules");
+
+    var options = {
+        node: true
+    };
+
+    lint.validateFileList(files.toArray(), options, {});
 });
