9f2a55b60bf919ccc58419c6ff2a96510edc8f1b
javieralmendrote <javier.almendrote@gmail.com>
2014-05-16 21:17:33 +0200

    Set up strict Lint options

--- Jakefile.js
+++ Jakefile.js
@@ -6,16 +6,33 @@ task("default", ["Lint"]);
 
 desc("Lint everything");
 
+function nodeLintOptions() {
+    var options = {
+        bitwise: true,
+        curly: false,
+        eqeqeq: true,
+        forin: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        noempty: true,
+        nonew: true,
+        regexp: true,
+        undef: true,
+        strict: true,
+        trailing: true,
+        node: true
+    };
+    return options;
+}
+
 task("Lint", [], function() {
     var lint = require("./build/lint/lint_runner.js");
 
     var files = new jake.FileList();
     files.include("**/*.js");
     files.exclude("node_modules");
-
-    var options = {
-        node: true
-    };
-
+    var options = nodeLintOptions();
     lint.validateFileList(files.toArray(), options, {});
 });
