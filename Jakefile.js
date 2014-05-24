3857cb25beb1b44dad1dd0848f300044b249136e
javieralmendrote <javier.almendrote@gmail.com>
2014-05-24 11:56:09 +0200

    Set up continuous integration script.

--- Jakefile.js
+++ Jakefile.js
@@ -1,38 +1,50 @@
 /*global desc, task, jake, fail, complete */
+    "use strict";
 
-"use strict";
+    desc("Build and test");
+    task("default", ["Lint"]);
 
-task("default", ["Lint"]);
+    desc("Lint everything");
 
-desc("Lint everything");
+    function nodeLintOptions() {
+        var options = {
+            bitwise: true,
+            curly: false,
+            eqeqeq: true,
+            forin: true,
+            immed: true,
+            latedef: true,
+            newcap: true,
+            noarg: true,
+            noempty: true,
+            nonew: true,
+            regexp: true,
+            undef: true,
+            strict: true,
+            trailing: true,
+            node: true
+        };
+        return options;
+    }
 
-function nodeLintOptions() {
-    var options = {
-        bitwise: true,
-        curly: false,
-        eqeqeq: true,
-        forin: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        noempty: true,
-        nonew: true,
-        regexp: true,
-        undef: true,
-        strict: true,
-        trailing: true,
-        node: true
-    };
-    return options;
-}
+    task("Lint", [], function () {
+        var lint = require("./build/lint/lint_runner.js");
 
-task("Lint", [], function() {
-    var lint = require("./build/lint/lint_runner.js");
+        var files = new jake.FileList();
+        files.include("**/*.js");
+        files.exclude("node_modules");
+        var options = nodeLintOptions();
+        lint.validateFileList(files.toArray(), options, {});
+    });
 
-    var files = new jake.FileList();
-    files.include("**/*.js");
-    files.exclude("node_modules");
-    var options = nodeLintOptions();
-    lint.validateFileList(files.toArray(), options, {});
-});
+    desc("Integrate");
+    task("integrate", ["default"], function () {
+        console.log("1. Make sure 'git status' is clean.");
+        console.log("2. Build on the integration box.");
+        console.log("   a. Walk over to integration box.");
+        console.log("   b. 'git pull'");
+        console.log("   c. 'jake'");
+        console.log("3. 'get checkout integration'");
+        console.log("4. 'git merge master --no-ff --log'");
+        console.log("5. 'git checkout master'");
+    });
