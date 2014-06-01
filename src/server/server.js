bfccc5571944b12e68d60966e20fa7b332641dcf
javieralmendrote <javier.almendrote@gmail.com>
2014-06-01 19:51:56 +0200

    Starting implementing teardown function.

--- src/server/server.js
+++ src/server/server.js
@@ -4,15 +4,18 @@
 "use strict";
 
 var http = require("http");
+var server;
 
 exports.start = function(){
-    var server = http.createServer();
+    server = http.createServer();
 
     server.on("request", function(request, response){
-        console.log("Received request");
-        response.end("foo");
+
     });
 
-
     server.listen(8080);    //TODO: remove duplication of port number
+};
+
+exports.stop = function(callback){
+    server.close(callback);
 };
