8e87bb10a09119b219cd11ca4e0f7bcfc6bc40bd
javieralmendrote <javier.almendrote@gmail.com>
2014-06-01 21:44:21 +0200

    Client/server working.

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -6,17 +6,29 @@
 var server = require ("./server.js");
 var http = require ("http");
 
-exports.tearDown = function(test) {
+exports.tearDown = function(done) {
     console.log("tear down start");
     server.stop(function() {
         console.log("stop callback");
         //todo: need test.done() equivalent here
+        done();
     });
     console.log("tear down end");
 };
 
 //TODO: handle case where stop() is called before start()
 //TODO: test-drive stop() callback
+
+//exports.testSpike = function(test) {
+//    server.start();
+//    http.get("http://localhost:8080/", function(res){
+//        console.log("Got response: " + res.statusCode);
+//        test.done();
+//        res.on("data", function(){});
+//    }).on('error', function(e){
+//        console.log("Got error: " + e.message);
+//    });
+//};
 
 exports.testHttpServer = function(test) {
     console.log("test started");
@@ -25,7 +37,9 @@ exports.testHttpServer = function(test) {
 
     http.get("http://localhost:8080", function(response) {
         console.log("response callback");
+        response.on("data", function(){});
         test.done();
+        console.log("test done");
     });
     console.log("get called");
 };
--- src/server/server.js
+++ src/server/server.js
@@ -7,15 +7,22 @@ var http = require("http");
 var server;
 
 exports.start = function(){
+    console.log("start called");
     server = http.createServer();
+    console.log("server called");
 
     server.on("request", function(request, response){
-
+        console.log("request called");
+        response.end();
     });
 
+    console.log("'on' called");
     server.listen(8080);    //TODO: remove duplication of port number
+
+    console.log("Listen called");
 };
 
 exports.stop = function(callback){
+    console.log("stop called");
     server.close(callback);
 };
