7bf2653328df528af6ab8964c660bf75cbc3a5b6
javieralmendrote <javier.almendrote@gmail.com>
2014-06-01 21:48:06 +0200

    Some clean code on our first client/server test working.

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -7,39 +7,15 @@ var server = require ("./server.js");
 var http = require ("http");
 
 exports.tearDown = function(done) {
-    console.log("tear down start");
     server.stop(function() {
-        console.log("stop callback");
-        //todo: need test.done() equivalent here
         done();
     });
-    console.log("tear down end");
 };
 
-//TODO: handle case where stop() is called before start()
-//TODO: test-drive stop() callback
-
-//exports.testSpike = function(test) {
-//    server.start();
-//    http.get("http://localhost:8080/", function(res){
-//        console.log("Got response: " + res.statusCode);
-//        test.done();
-//        res.on("data", function(){});
-//    }).on('error', function(e){
-//        console.log("Got error: " + e.message);
-//    });
-//};
-
-exports.testHttpServer = function(test) {
-    console.log("test started");
+exports.testServerRespondsToGetRequests = function(test) {
     server.start();
-    console.log("server started");
-
     http.get("http://localhost:8080", function(response) {
-        console.log("response callback");
         response.on("data", function(){});
         test.done();
-        console.log("test done");
     });
-    console.log("get called");
 };
