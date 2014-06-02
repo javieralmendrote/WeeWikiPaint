4e3eac5ec48f0bb150349ca18bbadffd4b4fb26a
javieralmendrote <javier.almendrote@gmail.com>
2014-06-02 20:20:44 +0200

    Added test to see if we throw exception if stop() is called and the server is not started, and when we call start without a port number.

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -6,18 +6,8 @@
 var server = require ("./server.js");
 var http = require ("http");
 
-exports.setUp = function(done){
-    server.start(8080);
-    done();
-};
-
-exports.tearDown = function(done) {
-    server.stop(function() {
-        done();
-    });
-};
-
 exports.test_serverReturnsHelloWorld = function(test) {
+    server.start(8080);
     var request = http.get("http://localhost:8080");
     request.on("response", function(response) {
         var receivedData = false;
@@ -30,20 +20,30 @@ exports.test_serverReturnsHelloWorld = function(test) {
         });
         response.on("end", function(){
             test.ok(receivedData, "should have received response data");
-            test.done();
+            server.stop(function(){
+                test.done();
+            });
         });
     });
 };
 
+exports.test_serverRequiresPortNumber = function(test){
+    test.throws(function(){
+        server.start();
+    });
+    test.done();
+};
+
 exports.test_serverRunsCallbackWhenStopCompletes = function(test) {
+    server.start(8080);
     server.stop(function(){
         test.done();
     });
-    server.start(); //TODO: chapuza para que pase el test
 };
 
-exports.test_stopCalledTwiceInARow = function(){
-    server.stop();
-    server.stop();
-    server.start();
+exports.test_stopCalledWhenServerIsntRunningThrowsException = function(test){
+    test.throws(function(){
+        server.stop();
+    });
+    test.done();
 };
